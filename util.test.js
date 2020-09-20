c02c81d0462feed1f4a56186b448fb4bf166c7c3
Monicacholico <monicachol@hotmail.com>
2020-09-19 22:26:33 -0500

    chapter 532

--- util.test.js
+++ util.test.js
@@ -1,6 +1,17 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText('Max', 29);
     expect(text).toBe('Max (29 years old)');
 });
+
+test('should output data-less text', () => {
+    const text = generateText('', null);
+    expect(text).toBe(' (null years old)');
+})
+
+test('should generate a valid text ouput', () => {
+    const text = checkAndGenerate('Max', 29);
+    expect(text).toBe('Max (29 years old)');
+
+})
